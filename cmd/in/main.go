b56685521a43469afc11670c48809fcd500e569e
ER Hapal <erwin.hapal@fairfaxmedia.co.nz>
2018-07-04 17:15:36 +1200

    removed versioning on in and check commands to avoid unnecessary db writes of resource-version

--- cmd/in/main.go
+++ cmd/in/main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
-	"time"
 )
 
 func main() {
-	os.Stdout.Write([]byte(fmt.Sprintf("{ \"version\" :{ \"ref\" :\"%d\"}}", time.Now().Unix())))
+	os.Stdout.Write([]byte(fmt.Sprintf("{ \"version\" :{ \"ref\" :\"none\"}}")))
 }

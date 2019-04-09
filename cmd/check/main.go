b3300ddb0ff77d481a1156d4284120bc9e82048c
lockan <andrew.lockhart@fairfaxmedia.co.nz>
2019-04-09 12:09:16 +1200

    Added check implementation using a version file and environment var

--- cmd/check/main.go
+++ cmd/check/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"os"
+	"path/filepath"
+	"io/ioutil"
 )
 
 type Version struct {
@@ -11,7 +13,32 @@ type Version struct {
 
 type Versions []Version
 
+func getVersionPathFromEnvironment() string {
+	VERSIONPATH, bExists := os.LookupEnv("VERSIONPATH")
+	if !bExists || VERSIONPATH == "" {
+		panic("$VERSIONPATH is empty or unset. Cannot retrieve resource verison")
+	}
+
+	abspath, err := filepath.Abs(filepath.Join(filepath.Dir(VERSIONPATH), "version"))
+	if err != nil {
+		panic(err)
+	}
+	return abspath
+}
+
+func versionFromFile(vpath string) (Version) {
+	version, err := ioutil.ReadFile(vpath)
+	if err != nil { panic (err) }
+	v := Version{}
+	v.Ref = string(version)
+	return v
+}
+
 func main() {
-	versions := Versions{}
-	json.NewEncoder(os.Stdout).Encode(versions)
+	versionlist := Versions{}
+	versionpath := getVersionPathFromEnvironment()
+	version := versionFromFile(versionpath)
+	versionlist = append(versionlist, version)
+	err := json.NewEncoder(os.Stdout).Encode(versionlist)
+	if err != nil { panic(err) }
 }
